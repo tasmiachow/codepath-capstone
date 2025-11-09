5132f8666c1bad49202d055e72c95f93f7561b5f
Krish A. Patel <krishpatel2067@gmail.com>
2025-11-09 11:01:07 -0500

    Add .editorconfig file and format GameCard

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -1,23 +1,23 @@
 import { Link } from "react-router-dom";
 
 function GameCard({ gameId, title, image, favorites }) {
-    const onFavoritePress = () => {
-        /* TODO */
-    };
+  const onFavoritePress = () => {
+    /* Send request to database to toggle favorite on/off */
+  };
 
-    return (
-        <div className="GameCard">
-            <img src={image} />
-            <h2>{title}</h2>
-            <div className="bottom-container">
-                <Link to={`/games/${gameId}`} role="button">Play</Link>
-                <button onClick={onFavoritePress}>
-                    <img src="TODO:fav-icon" />
-                    <span>{favorites}</span>
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="GameCard">
+      <img src={image} />
+      <h2>{title}</h2>
+      <div className="bottom-container">
+        <Link to={`/games/${gameId}`} role="button">Play</Link>
+        <button onClick={onFavoritePress}>
+          <img src="TODO:fav-icon" />
+          <span>{favorites}</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default GameCard;
